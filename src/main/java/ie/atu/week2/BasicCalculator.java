c274999a62d0303118ab11d383bb9d44a958c073
Billy Murphy <G00465240@atu.ie>
2025-09-23 14:29:14 +0100

    Taking in first number

--- src/main/java/ie/atu/week2/BasicCalculator.java
+++ src/main/java/ie/atu/week2/BasicCalculator.java
@@ -1,7 +1,15 @@
 package ie.atu.week2;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
     static void main() {
+        //Prompt for number
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Please enter first number: ");
+
+        double FirstNumber = scan1.nextDouble();
+        System.out.println(FirstNumber);
 
     }
 }
