1ec9ee17d0f3a7f3d4c72db93af551e9118cd857
Billy Murphy <G00465240@atu.ie>
2025-09-23 14:35:36 +0100

    Display results

--- src/main/java/ie/atu/week2/BasicCalculator.java
+++ src/main/java/ie/atu/week2/BasicCalculator.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 public class BasicCalculator {
     static void main() {
-        //Prompt for number
+        //Prompt for first number
         Scanner scan1 = new Scanner(System.in);
         System.out.println("Please enter first number: ");
 
         double FirstNumber = scan1.nextDouble();
-        System.out.println(FirstNumber);
+        System.out.println("You entered: " + FirstNumber);
 
+        //Prompt for second number
+        System.out.println("Please enter second number: ");
+
+        double SecondNumber = scan1.nextDouble();
+        System.out.println("You entered: " + SecondNumber);
+
+        //display results
+        double sum = FirstNumber + SecondNumber;
+        System.out.println("The sum is: " + sum);
     }
 }
