060912d0f7b695465731f949ba597cc7e920af7b
Billy Murphy <G00465240@atu.ie>
2025-09-24 16:53:12 +0100

    added the four operations

--- src/main/java/ie/atu/week2/BasicCalculator.java
+++ src/main/java/ie/atu/week2/BasicCalculator.java
@@ -14,11 +14,48 @@ public class BasicCalculator {
         //Prompt for second number
         System.out.println("Please enter second number: ");
 
+
         double SecondNumber = scan1.nextDouble();
         System.out.println("You entered: " + SecondNumber);
 
+
+        //decide operation
+        System.out.println("Choose an Operation(add, subtract, multiply, divide): ");
+        String operation = scan1.next();
+
+        //calculate results
+        double result = switch (operation)
+        {
+            case "add" -> {
+                yield FirstNumber + SecondNumber;
+            }
+
+
+            case "subtract" -> {
+                yield FirstNumber - SecondNumber;
+            }
+
+            case "multiply" -> {
+                yield FirstNumber * SecondNumber;
+            }
+            case "divide" -> {
+                if(SecondNumber == 0)
+                {
+                    System.out.println();
+                    yield 0;
+                }
+                else {
+                    yield FirstNumber / SecondNumber;
+                }
+            }
+
+            default ->{
+                System.out.println("Invalid Operation");
+                yield 0;
+            }
+
+        };
         //display results
-        double sum = FirstNumber + SecondNumber;
-        System.out.println("The sum is: " + sum);
+        System.out.println("Results: " + result);
     }
 }
